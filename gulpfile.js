621018758b3840e44058e345bbec883c5b5c0a11
Steven Bister <s.bister@bham.ac.uk>
2017-11-14 15:02:09 +0000

    added images folder to build and placed paths into an object to make it easier if they change

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// Dependencies
 var gulp  = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
@@ -12,6 +13,19 @@ var gulp  = require('gulp'),
     del = require('del'),
     panini = require('panini'),
     siphon = require('siphon-media-query');
+
+// Paths
+var paths = {
+  app: 'app/**/*',
+  appHTML: 'app/**/*.html',
+  appSass: 'app/assets/sass/**/*.scss',
+  appIMG: 'app/assets/images/**/*',
+
+  build: 'build',
+  buildHTML: 'build/**/*.html',
+  buildCSS: 'build/css/',
+  buildIMG: 'build/images/'
+}
 
 //compile pages, layouts and partials
 gulp.task('pages', function() {
@@ -34,29 +48,34 @@ gulp.task('pages:reset', function(done) {
 
 //compile sass
 gulp.task('sass', function() {
-  return gulp.src('app/assets/sass/**/*.scss')
+  return gulp.src(paths.appSass)
   .pipe(sourcemaps.init())
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('build/css'))
   .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest('build/css'));
 });
 gulp.task('sass:watch', function() {
-  gulp.watch('app/assets/sass/**/*.scss', ['sass']);
+  gulp.watch(paths.appSass, ['sass']);
+});
+
+//copy images into build folder
+gulp.task('copyImages', function() {
+  return gulp.src(paths.appIMG)
+  .pipe(gulp.dest(paths.buildIMG));
 });
 
 //watch files and launch browser-sync
-gulp.task('watch', ['sass:watch', 'pages'], function() {
+gulp.task('watch', ['sass:watch', 'pages', 'copyImages'], function() {
   var files = [
-      'app/**/*.html',
-      'app/assets/img/**/*.{png,jpg,gif,svg,webp}',
-      'build/css/**/*.css'
+      paths.appHTML,
+      paths.appIMG,
+      paths.buildCSS + '**/*.css'
   ];
 
   browserSync.init(files, {
     notify: false,
     server: {
-      baseDir: "build"
+      baseDir: paths.build
     }
   });
   //watch panini files and refresh on save
@@ -70,18 +89,18 @@ gulp.task('deleteDistFolder', function() {
 
 //compress images
 gulp.task('compressImages', function() {
-  gulp.src('app/assets/img/**/*')
+  gulp.src(paths.buildIMG + '**/*')
   .pipe(imagemin({
     verbose: true
   }))
-  .pipe(gulp.dest('dist/img'))
+  .pipe(gulp.dest('dist/images'))
 });
 
 //remove unused css selectors and replace double quotes with single quotes
 gulp.task('tidycss', function() {
-  return gulp.src('build/css/main.css')
+  return gulp.src(paths.buildCSS + 'main.css')
   .pipe(uncss({
-    html: ['build/**/*.html'],
+    html: [paths.buildHTML],
     //list selectors for unused css to ignore e.g. client specific selectors
     ignore: [
       '#outlook a',
@@ -98,16 +117,16 @@ gulp.task('tidycss', function() {
   .pipe(replaceQuotes({
     quote: 'single'
   }))
-  .pipe(gulp.dest('build/css'));
+  .pipe(gulp.dest(paths.buildCSS));
 });
 
 //insert css remove link to external sheet & embed media queries
 gulp.task('insert', function() {
   //set stylesheets to stings and siphon out media queries
-  var css = fs.readFileSync('build/css/main.css').toString(),
+  var css = fs.readFileSync(paths.buildCSS + 'main.css').toString(),
       mediaQuery = siphon(css);
 
-  return gulp.src('build/**/*.html')
+  return gulp.src(paths.buildHTML)
   .pipe(replace('<!-- <style> -->', '<style amf:inline>' + css + '</style>'))
   .pipe(replace('<!-- <media queries> -->', '<style>' + mediaQuery + '</style>'))
   .pipe(replace('<link rel="stylesheet" type="text/css" href="css/main.css">', ''))
