43bdacad188861d2d053d3e2bef94ffde2afa23a
Steven Bister <s.bister@bham.ac.uk>
2019-10-30 13:27:15 +0000

    started added styles function

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@
 const { src, dest, watch, series, parallel } = require('gulp')
 const browserSync = require('browser-sync').create()
 const panini = require('panini')
+const sourcemaps = require('gulp-sourcemaps')
+const postcss = require('gulp-postcss')
+const postcssVars = require('postcss-simple-vars')
 
-// TODO: compile, tidy & insert css files
-// TODO:  create production ready function
+// TODO: create production ready function
 
-// Compile pages, layouts and partials into one file
+// Compile pages, layouts and partials into one file in the build dir
 function compilePages () {
   // Return all html files in pages dir/sub dir
   return src('src/pages/**/*.html')
@@ -15,17 +17,46 @@ function compilePages () {
     .pipe(panini({
       root: 'src/pages/',
       layouts: 'src/layouts/',
-      pageLayouts: {
-        // https://github.com/foundation/panini#pagelayouts
-        // All pages within src/pages/[foldername] will use the equlivalent layout e.g.
-        // 'newsletter': 'newsletter'
-      },
+      /**
+       * https://github.com/foundation/panini#pagelayouts
+       * All pages within src/pages/[foldername] will use the equlivalent layout e.g.
+       * Uncomment the lines below to use
+       */
+      // pageLayouts: {
+      //   'newsletter': 'newsletter'
+      // },
       partials: 'src/partials/'
     }))
     // Push compiled files into the build dir
     .pipe(dest('build/'))
+    .on('finish', browserSync.reload)
 }
 exports.compilePages = compilePages
+
+// Refresh pages to rebuild layouts etc. this needs to happen as the pages task will not refresh on it's own
+function refreshPanini (done) {
+  panini.refresh()
+  done()
+}
+
+// Compile, tidy & insert css files
+function styles () {
+  const plugins = [
+    postcssVars
+  ]
+  // Return css, pcss or postcss files
+  // TODO: Might be nice to check for pcss files too
+  // TODO: Add other postcss plugins
+  return src('src/styles/**/*.css')
+    // Init sourcemap 
+    .pipe(sourcemaps.init())
+    // Run postcss
+    .pipe(postcss(plugins))
+    // // Write sourcemap
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest('build/styles/'))
+}
+exports.styles = styles
 
 // Browsersync
 function browsersync () {
@@ -36,5 +67,11 @@ function browsersync () {
       baseDir: 'build/'
     }
   })
+
+  // Watch html files in pages dir and run compilePages func parallel
+  watch('src/pages/**/*.html', parallel(compilePages))
+  watch('src/styles/**/*.css', parallel(styles))
+  // Watch panini files and refresh on save
+  watch(['src/{root,layout,pages,partials}/**/*'], series(refreshPanini))
 }
 exports.browsersync = browsersync
